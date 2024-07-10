ede6d42a2d2a42b2bbeb48b6770c188ee8b92c11
Lowell Bartlett <bartlett.lowell@gmail.com>
2024-07-10 12:04:52 -0700

    Initialize analysis repo for experimental pipelines
    Also upload first function in DMTS_tri processing pipeline

---
+++ Analyses/DMTS_Tri_LabGym/DMTS_tri_pipeline_modular.m
@@ -0,0 +1,7 @@
+% Modular script for analyses of DMTS_tri behavior data.
+% Lowell Bartlett  ***  7/10/2024
+
+
+%% Create class objects by filtering pre-organized data
+[trainingParser, testingSessions] = DMTS_tri_pipeline_init;
+
