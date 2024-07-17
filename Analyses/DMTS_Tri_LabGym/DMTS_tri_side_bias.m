be5fe84232ffdbbd898ab938899c6fa8e2cc45d9
Lowell Bartlett <bartlett.lowell@gmail.com>
2024-07-17 11:49:35 -0700

    Improve speed of side bias calculation

--- Analyses/DMTS_Tri_LabGym/DMTS_tri_side_bias.m
+++ Analyses/DMTS_Tri_LabGym/DMTS_tri_side_bias.m
@@ -1,9 +1,8 @@
 function biasIdx = DMTS_tri_side_bias(sessionParser)
-
-leftTrials = [1 4 5];
-leftTrialsIdx = ismember(sessionParser.session.TrialTypes, leftTrials);
-rightTrials = [2 3 6];
-rightTrialsIdx = ismember(sessionParser.session.TrialTypes, rightTrials);
+leftTrials = sessionParser.config.trialTypes.Left;
+rightTrials = sessionParser.config.trialTypes.Right;
+leftTrialsIdx = sessionParser.trial_intersection_BpodParser('trialType', 'Left');
+rightTrialsIdx = sessionParser.trial_intersection_BpodParser('trialType', 'Right');
 
 choicePort = {'Port1In', 'Port2In', 'Port3In'};
 
@@ -11,17 +10,19 @@ waitForChoiceState = sessionParser.state_times('WaitForChoicePoke');
 choiceMade = cellfun(@(x) x(end, 2), waitForChoiceState);
 leftChosen = zeros(1, sessionParser.session.nTrials);
 rightChosen = zeros(1, sessionParser.session.nTrials);
+
+choiceTimeAll = cellfun(@(x) sessionParser.event_times('event', x, ...
+    'withinState', 'WaitForChoicePoke', 'trialized', true), choicePort, 'uni', 0);
+
 for lt = find(leftTrialsIdx)
     portIdx = ismember(leftTrials, sessionParser.session.TrialTypes(lt));
-    leftChoice = choicePort{portIdx};
-    leftChoiceTime = sessionParser.event_times('event', leftChoice, 'withinState', 'WaitForChoicePoke', 'trials', lt);
+    leftChoiceTime = choiceTimeAll{portIdx}(lt);
     leftChosen(lt) = any(ismember(leftChoiceTime{1}, choiceMade(lt)));
     rightChosen(lt) = ~any(ismember(leftChoiceTime{1}, choiceMade(lt)));
 end
 for rt = find(rightTrialsIdx)
     portIdx = ismember(rightTrials, sessionParser.session.TrialTypes(rt));
-    rightChoice = choicePort{portIdx};
-    rightChoiceTime = sessionParser.event_times('event', rightChoice, 'withinState', 'WaitForChoicePoke', 'trials', rt);
+    rightChoiceTime = choiceTimeAll{portIdx}(rt);
     rightChosen(rt) = any(ismember(rightChoiceTime{1}, choiceMade(rt)));
     leftChosen(rt) = ~any(ismember(rightChoiceTime{1}, choiceMade(rt)));
 
