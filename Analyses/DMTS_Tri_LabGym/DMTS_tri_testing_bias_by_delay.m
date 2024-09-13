d1261df0717211a78f6e304da0a8d53e33ff8373
Lowell Bartlett <bartlett.lowell@gmail.com>
2024-09-13 13:14:52 -0700

    Add rough version of testing bias by delay
    DMTS_tri

---
+++ Analyses/DMTS_Tri_LabGym/DMTS_tri_testing_bias_by_delay.m
@@ -0,0 +1,13 @@
+function DMTS_tri_testing_bias_by_delay(testingSessions, delayBins)
+
+biasByAnimal = arrayfun(@(x) side_bias_by_delay(x.bpod, delayBins), testingSessions.sessions, 'uni', 0);
+biasByDelay = cell(1, numel(delayBins));
+for d = 1:numel(delayBins)
+    biasByDelay{d} = cellfun(@(x) abs(x{d}), biasByAnimal);
+end
+biasByDelay = cat(1, biasByDelay{:})';
+bar_and_error(biasByDelay, 3)
+clean_DMTS_figs
+xticklabels({'0-3', '3-5', '5-7'})
+xlabel('Delay Bins')
+ylabel('Absolute bias')
